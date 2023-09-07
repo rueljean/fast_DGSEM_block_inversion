ea8f76d9935255650bb748ddba7b8a00da680769
Riccardo Milani <riccardo.milani@onera.fr>
2023-09-07 11:42:14 +0200

    test: Check exactness of matrix operations

--- test_fast_dgsem.py
+++ test_fast_dgsem.py
@@ -5,14 +5,13 @@ import fast_DGSEM_block_inversion as f_dgsem
 
 
 @pytest.mark.parametrize("p", [2, 3, 4, 5, 6])
-@pytest.mark.parametrize("lambda_x", [1.0, 2.0])
-@pytest.mark.parametrize("lambda_y", [1.0])
 class TestMethods:
-    def test_eigen(self, p, lambda_x, lambda_y):
-        # lambda_x/y not useful here, but we keep to have the parametrize
+    def test_eigen(self, p):
         D = f_dgsem.D_matrix(p)
         assert np.allclose(f_dgsem.eigen_L_numpy(D), f_dgsem.eigen_L_analytical(D))
 
+    @pytest.mark.parametrize("lambda_x", [1.0, 2.0])
+    @pytest.mark.parametrize("lambda_y", [1.0])
     def test_2D_inversion(self, p, lambda_x, lambda_y):
         D = f_dgsem.D_matrix(p)
         M = 0.5 * np.diag(f_dgsem.LOBATTO_WEIGHTS_BY_ORDER[p])
@@ -21,6 +20,8 @@ class TestMethods:
             f_dgsem.L2d_inversion_analytical(D, M, lambda_x, lambda_y),
         )
 
+    @pytest.mark.parametrize("lambda_x", [1.0, 2.0])
+    @pytest.mark.parametrize("lambda_y", [1.0])
     def test_2D_inversion_viscosity(self, p, lambda_x, lambda_y):
         D = f_dgsem.D_matrix(p)
         M = 0.5 * np.diag(f_dgsem.LOBATTO_WEIGHTS_BY_ORDER[p])
@@ -28,3 +29,27 @@ class TestMethods:
             f_dgsem.L2d_inversion_viscosity_numpy(D, M, lambda_x, lambda_y),
             f_dgsem.L2d_inversion_viscosity_analytical(D, M, lambda_x, lambda_y),
         )
+
+
+class TestMatrix:
+    # Default dimension
+    N = 5
+    # Default random number generator
+    rng = np.random.default_rng()
+
+    def test_diagonal_matrix_multiply(self):
+        diag = self.rng.random(self.N)
+        A = np.diag(diag)
+        B = self.rng.random((self.N, self.N))
+        assert np.allclose(f_dgsem.diagonal_matrix_multiply(diag, B), A.dot(B))
+
+    def test_diagonal_auto_kron(self):
+        diag = self.rng.random(self.N)
+        A = np.diag(diag)
+        assert np.allclose(np.diag(f_dgsem.diagonal_auto_kron(diag)), np.kron(A, A))
+
+    @pytest.mark.parametrize("val", [3.0, None])
+    def test_diagonal_add(self, val):
+        A = self.rng.random((self.N, self.N))
+        d_val = val * np.ones(self.N) if val is not None else self.rng.random(self.N)
+        assert np.allclose(f_dgsem.diagonal_add(A, d_val), A + np.diag(d_val))
